14bf740e785769a7ceb303d9c25a16070842411a
winger <wuwenhui@rightchaintech.com>
2025-01-18 21:47:27 +0800

    fix HelleNets

--- examples/HelloNets/src/main.zig
+++ examples/HelloNets/src/main.zig
@@ -1,12 +1,9 @@
 const std = @import("std");
 const zguilite = @import("zguilite");
 const x11 = @import("x11");
-const _3d = @import("./3d.zig");
 const UI_WIDTH: i32 = 480; // 示例值，根据实际情况修改
 const UI_HEIGHT: i32 = 320; // 示例值，根据实际情况修改
-const SHAPE_SIZE = 50;
 const Z_ORDER_LEVEL_0 = zguilite.Z_ORDER_LEVEL_0;
-const SHAPE_CNT = 2;
 const Main = struct {
     wnd: zguilite.Wnd = .{ .m_class = "Main", .m_vtable = .{
         .on_paint = Main.on_paint,
@@ -19,194 +16,175 @@ const Main = struct {
     }
 };
 
-fn ShapeClass(T: type) type {
-    const NewType = struct {
-        obj: T,
-        const Self = @This();
-        pub fn init(t: anytype) Self {
-            return .{
-                .obj = t,
-            };
-        }
-        pub fn draw(this: *Self, x: i32, y: i32, isErase: bool) void {
-            this.obj.draw(x, y, isErase);
-        }
-        pub fn rotate(this: *Self) void {
-            this.obj.rotate();
-        }
-    };
-    return NewType;
-}
+const POINT_COL = 25;
+const POINT_ROW = 20;
+const X_SPACE = 18;
+const Y_SPACE = 25;
+const STRING_LENGTH = 12;
 
-fn Shape(sub: anytype) ShapeClass(@TypeOf(sub)) {
-    const T = ShapeClass(@TypeOf(sub));
-    return T.init(sub);
-}
+const FRICTION: f32 = 0.98;
+const GRAVITY_ACC: f32 = 0.05;
+const DELTA_TIME: f32 = 0.1;
 
-var s_surface: *zguilite.Surface = undefined;
+var s_surface:*zguilite.Surface = undefined;
+pub const c_point = struct {
+    x: f32,
+    y: f32,
+    vx: f32,
+    vy: f32,
+    last_x: f32,
+    last_y: f32,
+    fixed: bool,
 
-const Cube = struct {
-    const Self = @This();
-    const points: [8][3]f64 = blk: {
-        var arr: [8][3]f64 = undefined;
-        arr[0] = .{ -SHAPE_SIZE, -SHAPE_SIZE, -SHAPE_SIZE };
-        arr[1] = .{ SHAPE_SIZE, -SHAPE_SIZE, -SHAPE_SIZE };
-        arr[2] = .{ SHAPE_SIZE, SHAPE_SIZE, -SHAPE_SIZE };
-        arr[3] = .{ -SHAPE_SIZE, SHAPE_SIZE, -SHAPE_SIZE };
-        arr[4] = .{ -SHAPE_SIZE, -SHAPE_SIZE, SHAPE_SIZE };
-        arr[5] = .{ SHAPE_SIZE, -SHAPE_SIZE, SHAPE_SIZE };
-        arr[6] = .{ SHAPE_SIZE, SHAPE_SIZE, SHAPE_SIZE };
-        arr[7] = .{ -SHAPE_SIZE, SHAPE_SIZE, SHAPE_SIZE };
-        break :blk arr;
-    };
-    points2d: [8][2]f64 = undefined,
-    angle: f64 = 0.5,
+    pub fn set(self: *c_point, x: f32, y: f32, fixed: bool) void {
+        self.x = x;
+        self.y = y;
+        self.vx = 0;
+        self.vy = 0;
+        self.last_x = x;
+        self.last_y = y;
+        self.fixed = fixed;
+    }
 
-    pub fn draw(self: *Self, x: i32, y: i32, isErase: bool) void {
-        var points2d: [8][2]i32 = undefined;
-        for (0..8) |i| {
-            points2d[i][0] = @intFromFloat(self.points2d[i][0]);
-            points2d[i][1] = @intFromFloat(self.points2d[i][1]);
+    pub fn update_location(self: *c_point, dt: f32) void {
+        if (self.fixed) return;
+        self.last_x = self.x;
+        self.last_y = self.y;
+        s_surface.draw_pixel(@intFromFloat(self.x), @intFromFloat(self.y), zguilite.GL_RGB(0, 0, 0),.Z_ORDER_LEVEL_0);
+        self.vx *= FRICTION;
+        self.vy *= FRICTION;
+        self.x += self.vx * dt;
+        self.y += self.vy * dt;
+        s_surface.draw_pixel(@intFromFloat(self.x), @intFromFloat(self.y), zguilite.GL_RGB(255, 255, 255),.Z_ORDER_LEVEL_0);
+    }
+
+    pub fn distance(self: *const c_point, p: *const c_point) f32 {
+        const dx = self.x - p.x;
+        const dy = self.y - p.y;
+        return std.math.sqrt(dx * dx + dy * dy);
+    }
+};
+pub const c_string = struct {
+    p1: *c_point,
+    p2: *c_point,
+    length: f32,
+
+    pub fn init(p1: *c_point, p2: *c_point, length: f32) c_string {
+        return .{
+            .p1 = p1,
+            .p2 = p2,
+            .length = length,
+        };
+    }
+
+    pub fn update_point_velocity(self: *c_string) void {
+        const force = (self.p1.distance(self.p2) - self.length) / 2;
+        const dx = self.p1.x - self.p2.x;
+        const dy = self.p1.y - self.p2.y;
+        const d = std.math.sqrt(dx * dx + dy * dy);
+
+        const nx = dx / d;
+        const ny = dy / d;
+
+        if (!self.p1.fixed) {
+            self.p1.vx -= nx * force;
+            self.p1.vy -= ny * force;
+            self.p1.vy += GRAVITY_ACC;
         }
-        for (0..4) |i| {
-            s_surface.draw_line(
-                points2d[i][0] + x,
-                points2d[i][1] + y,
-                points2d[(i + 1) % 4][0] + x,
-                points2d[(i + 1) % 4][1] + y,
-                if (isErase) 0 else 0xffff0000,
-                0,
-            );
-            s_surface.draw_line(
-                points2d[i + 4][0] + x,
-                points2d[i + 4][1] + y,
-                points2d[((i + 1) % 4) + 4][0] + x,
-                points2d[((i + 1) % 4) + 4][1] + y,
-                if (isErase) 0 else 0xff00ff00,
-                0,
-            );
-            s_surface.draw_line(
-                points2d[i][0] + x,
-                points2d[i][1] + y,
-                points2d[(i + 4)][0] + x,
-                points2d[(i + 4)][1] + y,
-                if (isErase) 0 else 0xffffff00,
-                0,
-            );
+
+        if (!self.p2.fixed) {
+            self.p2.vx += nx * force;
+            self.p2.vy += ny * force;
+            self.p2.vy += GRAVITY_ACC;
         }
     }
 
-    pub fn rotate(self: *Self) void {
-        var rotateOut1: [3]f64 = undefined;
-        var rotateOut2: [3]f64 = undefined;
-        var rotateOut3: [3]f64 = undefined;
-        for (0..8) |i| {
-            _3d.rotateX(self.angle, &Cube.points[i], &rotateOut1);
-            _3d.rotateY(self.angle, &rotateOut1, &rotateOut2);
-            _3d.rotateZ(self.angle, &rotateOut2, &rotateOut3);
-            _3d.projectOnXY(&rotateOut3, &self.points2d[i], 1);
+    pub fn draw(self: *c_string) void {
+        if (self.p1.x == self.p1.last_x and self.p1.y == self.p1.last_y and
+            self.p2.x == self.p2.last_x and self.p2.y == self.p2.last_y) {
+            return;
         }
-        self.angle += 0.1;
+
+        s_surface.draw_line(@intFromFloat(self.p1.last_x),
+         @intFromFloat(self.p1.last_y),
+         @intFromFloat(self.p2.last_x),
+         @intFromFloat(self.p2.last_y),
+         zguilite.GL_RGB( 0, 0, 0),Z_ORDER_LEVEL_0);
+        s_surface.draw_line(@intFromFloat(self.p1.x), 
+        @intFromFloat(self.p1.y), 
+        @intFromFloat(self.p2.x), 
+        @intFromFloat(self.p2.y),
+         zguilite.GL_RGB(255, 255, 255),Z_ORDER_LEVEL_0);
     }
 };
 
-const Pyramid = struct {
-    const Self = @This();
-    const points: [5][3]f64 = blk: {
-        var arr: [5][3]f64 = undefined;
-        arr[0] = .{ 0, -SHAPE_SIZE, 0 };
-        arr[1] = .{ -SHAPE_SIZE, SHAPE_SIZE, -SHAPE_SIZE };
-        arr[2] = .{ SHAPE_SIZE, SHAPE_SIZE, -SHAPE_SIZE };
-        arr[3] = .{ SHAPE_SIZE, SHAPE_SIZE, SHAPE_SIZE };
-        arr[4] = .{ -SHAPE_SIZE, SHAPE_SIZE, SHAPE_SIZE };
-        break :blk arr;
-    };
-    points2d: [5][2]f64 = undefined,
-    angle: f64 = 0.5,
+var points:[POINT_COL][POINT_ROW]c_point = undefined;
+var strings: [((POINT_COL - 1) * POINT_ROW + POINT_COL * (POINT_ROW - 1))]c_string = undefined;
 
-    pub fn draw(self: *Self, _x: i32, _y: i32, isErase: bool) void {
-        const x: f64 = @floatFromInt(_x);
-        const y: f64 = @floatFromInt(_y);
-
-        const color: u32 = if (isErase) 0 else 0xff007acc;
-        s_surface.draw_line(
-            @intFromFloat(self.points2d[0][0] + x),
-            @intFromFloat(self.points2d[0][1] + y),
-            @intFromFloat(self.points2d[1][0] + x),
-            @intFromFloat(self.points2d[1][1] + y),
-            color,
-            Z_ORDER_LEVEL_0,
-        );
-        s_surface.draw_line(
-            @intFromFloat(self.points2d[0][0] + x),
-            @intFromFloat(self.points2d[0][1] + y),
-            @intFromFloat(self.points2d[2][0] + x),
-            @intFromFloat(self.points2d[2][1] + y),
-            color,
-            Z_ORDER_LEVEL_0,
-        );
-        s_surface.draw_line(
-            @intFromFloat(self.points2d[0][0] + x),
-            @intFromFloat(self.points2d[0][1] + y),
-            @intFromFloat(self.points2d[3][0] + x),
-            @intFromFloat(self.points2d[3][1] + y),
-            color,
-            Z_ORDER_LEVEL_0,
-        );
-        s_surface.draw_line(
-            @intFromFloat(self.points2d[0][0] + x),
-            @intFromFloat(self.points2d[0][1] + y),
-            @intFromFloat(self.points2d[4][0] + x),
-            @intFromFloat(self.points2d[4][1] + y),
-            color,
-            Z_ORDER_LEVEL_0,
-        );
-
-        s_surface.draw_line(
-            @intFromFloat(self.points2d[1][0] + x),
-            @intFromFloat(self.points2d[1][1] + y),
-            @intFromFloat(self.points2d[2][0] + x),
-            @intFromFloat(self.points2d[2][1] + y),
-            color,
-            Z_ORDER_LEVEL_0,
-        );
-        s_surface.draw_line(
-            @intFromFloat(self.points2d[2][0] + x),
-            @intFromFloat(self.points2d[2][1] + y),
-            @intFromFloat(self.points2d[3][0] + x),
-            @intFromFloat(self.points2d[3][1] + y),
-            color,
-            Z_ORDER_LEVEL_0,
-        );
-        s_surface.draw_line(
-            @intFromFloat(self.points2d[3][0] + x),
-            @intFromFloat(self.points2d[3][1] + y),
-            @intFromFloat(self.points2d[4][0] + x),
-            @intFromFloat(self.points2d[4][1] + y),
-            color,
-            Z_ORDER_LEVEL_0,
-        );
-        s_surface.draw_line(
-            @intFromFloat(self.points2d[4][0] + x),
-            @intFromFloat(self.points2d[4][1] + y),
-            @intFromFloat(self.points2d[1][0] + x),
-            @intFromFloat(self.points2d[1][1] + y),
-            color,
-            Z_ORDER_LEVEL_0,
-        );
+pub fn trigger(x: i32, y: i32, is_down: bool) void {
+    if (is_down) {
+        points[POINT_COL / 2][POINT_ROW / 2].set(@floatFromInt(x), @floatFromInt(y), true);
+    } else {
+        points[POINT_COL / 2][POINT_ROW / 2].set(@floatFromInt(x), @floatFromInt(y), false);
     }
-    pub fn rotate(self: *Self) void {
-        var rotateOut1: [3]f64 = undefined;
-        var rotateOut2: [3]f64 = undefined;
-        for (0..5) |i| {
-            _3d.rotateY(self.angle, &Pyramid.points[i], &rotateOut1);
-            _3d.rotateX(0.1, &rotateOut1, &rotateOut2);
-            const zFactor = SHAPE_SIZE / (2.2 * SHAPE_SIZE - rotateOut2[2]);
-            _3d.projectOnXY(&rotateOut2, &self.points2d[i], zFactor);
+}
+
+pub fn run() !void {
+
+    // 初始化点
+    for (0..POINT_ROW) |y| {
+        for (0..POINT_COL) |x| {
+            const fx:f32 = @floatFromInt(x);
+            const fy:f32 = @floatFromInt(y);
+            points[x][y].set(X_SPACE * 2 + fx * X_SPACE, Y_SPACE * 2 + fy * Y_SPACE, y == 0);
         }
-        self.angle += 0.1;
     }
-};
+
+    // 初始化线
+    var sum: usize = 0;
+    for (0..POINT_ROW) |y| {
+        for (0..POINT_COL - 1) |x| {
+            strings[sum] = c_string.init(&points[x][y], &points[x + 1][y], STRING_LENGTH);
+            sum += 1;
+        }
+    }
+    for (0..POINT_ROW - 1) |y| {
+        for (0..POINT_COL) |x| {
+            strings[sum] = c_string.init(&points[x][y], &points[x][y + 1], STRING_LENGTH);
+            sum += 1;
+        }
+    }
+    std.debug.assert(sum == strings.len);
+
+    var count: usize = 0;
+
+    // 更新
+    while (true) {
+        for (0..sum) |i| {
+            strings[i].update_point_velocity();
+        }
+
+        for (0..POINT_ROW) |y| {
+            for (0..POINT_COL) |x| {
+                points[x][y].update_location(DELTA_TIME);
+            }
+        }
+
+        for (0..sum) |i| {
+            strings[i].draw();
+        }
+
+        std.time.sleep(10 * std.time.ns_per_ms);
+
+        // 自动触发（仅限MCU）
+        if (count % 500 == 0) {
+            trigger(0, 0, true);
+            trigger(0, 0, false);
+        }
+        count += 1;
+        try x11.refreshApp();
+    }
+}
 pub fn main() !void {
     std.log.debug("main begin", .{});
     try loadResource();
@@ -225,6 +203,7 @@ pub fn main() !void {
     try _display.init2(frameBuffer.ptr, screen_width, screen_height, screen_width, screen_height, color_bytes, 3, null);
     var surface = try _display.alloSurface(.Z_ORDER_LEVEL_1, zguilite.Rect.init2(0, 0, screen_width, screen_height));
     surface.set_active(true);
+    s_surface = surface;
 
     const rect = zguilite.Rect.init2(0, 0, UI_WIDTH, UI_HEIGHT);
 
@@ -242,37 +221,9 @@ pub fn main() !void {
             _ = user; // autofix
             _ = id;
             _ = param;
-
-            var theCube: [SHAPE_CNT]Cube = undefined;
-            var thePyramid: [SHAPE_CNT]Pyramid = undefined;
-            for (0..SHAPE_CNT) |i| {
-                theCube[i].rotate();
-                thePyramid[i].rotate();
-            }
-            while (true) {
-
-                for (0..SHAPE_CNT) |i| {
-                    const ii = @as(i32, @intCast(@as(u32, @truncate(i))));
-                    
-                    theCube[i].draw(120 + ii * 240, 100, true); // erase footprint
-                    theCube[i].rotate();
-                    theCube[i].draw(120 + ii * 240, 100, false); // refresh cube
-                    
-                    thePyramid[i].draw(120 + ii * 240, 250, true); // erase footprint
-                    thePyramid[i].rotate();
-                    thePyramid[i].draw(120 + ii * 240, 250, false); // refresh pyramid
-                }
-                try x11.refreshApp();
-                std.time.sleep(50 * std.time.ns_per_ms);
-            }
+            try run();
         }
     }.onIdle);
-    s_surface = surface;
-    // var cube = Cube{};
-    // cube.angle = 0.8;
-    // cube.rotate();
-    // cube.draw(100, 100, false);
-
     try x11.appLoop();
 
     std.log.err("main exited", .{});
